41aff9ef510d0669955f80d2a5b3b71062ddd312
MichielP1807 <michielp1807@gmail.com>
2024-06-13 13:55:03 +0200

    Small performance improvements

--- betterblittle.lua
+++ betterblittle.lua
@@ -4,8 +4,11 @@ local floor = math.floor
 local min = math.min
 local concat = table.concat
 
+local colorMap = {}
+for i = 1, 16 do colorMap[2 ^ (i - 1)] = i end
+
 local colorChar = {}
-for i = 1, 16 do colorChar[2 ^ (i - 1)] = ("0123456789abcdef"):sub(i, i) end
+for i = 1, 16 do colorChar[i] = ("0123456789abcdef"):sub(i, i) end
 
 local colorDistances
 
@@ -77,16 +80,10 @@ local function computeColorDistances(window, colorSpace)
 		for c2 = 1, 16 do
 			local r2, g2, b2 = window.getPaletteColor(2 ^ (c2 - 1))
 			if colorSpace ~= "sRGB" then r2, g2, b2 = sRGBtoOklab(r2, g2, b2) end
-			local d = (r2 - r1) ^ 2 + (g2 - g1) ^ 2 + (b2 - b1) ^ 2
-			distances[2 ^ (c2 - 1)] = d
+			distances[c2] = (r2 - r1) ^ 2 + (g2 - g1) ^ 2 + (b2 - b1) ^ 2
 		end
-		colorDistances[2 ^ (c1 - 1)] = distances
+		colorDistances[c1] = distances
 	end
-end
-
-local function colorCloser(target, c1, c2)
-	local dists = colorDistances[target]
-	return dists[c1] < dists[c2]
 end
 
 local char = string.char
@@ -94,14 +91,15 @@ local allChars = {}
 for i = 128, 128+31 do allChars[i] = char(i) end
 local bxor = bit.bxor
 local function getCharFomPixelGroup(c1, c2, p1, p2, p3, p4, p5, p6)
-	local cc = colorCloser
+	local c1Dist = colorDistances[c1]
+	local c2Dist = colorDistances[c2]
 	local charNr = 128
-	if p1 == c1 or p1 ~= c2 and cc(p1, c1, c2) then charNr = charNr + 1 end
-	if p2 == c1 or p2 ~= c2 and cc(p2, c1, c2) then charNr = charNr + 2 end
-	if p3 == c1 or p3 ~= c2 and cc(p3, c1, c2) then charNr = charNr + 4 end
-	if p4 == c1 or p4 ~= c2 and cc(p4, c1, c2) then charNr = charNr + 8 end
-	if p5 == c1 or p5 ~= c2 and cc(p5, c1, c2) then charNr = charNr + 16 end
-	if p6 == c1 or p6 ~= c2 and cc(p6, c1, c2) then
+	if p1 == c1 or p1 ~= c2 and c1Dist[p1] < c2Dist[p1] then charNr = charNr + 1 end
+	if p2 == c1 or p2 ~= c2 and c1Dist[p2] < c2Dist[p2] then charNr = charNr + 2 end
+	if p3 == c1 or p3 ~= c2 and c1Dist[p3] < c2Dist[p3] then charNr = charNr + 4 end
+	if p4 == c1 or p4 ~= c2 and c1Dist[p4] < c2Dist[p4] then charNr = charNr + 8 end
+	if p5 == c1 or p5 ~= c2 and c1Dist[p5] < c2Dist[p5] then charNr = charNr + 16 end
+	if p6 == c1 or p6 ~= c2 and c1Dist[p6] < c2Dist[p6] then
 		return allChars[bxor(31, charNr)], true
 	end
 	return allChars[charNr], false
@@ -130,12 +128,12 @@ local function drawBuffer(buffer, win)
 		for x = 1, maxX do
 			local ox = (x-1) * 2 + 1
 
-			local p1 = r1[ox]
-			local p2 = r1[ox+1]
-			local p3 = r2[ox]
-			local p4 = r2[ox+1]
-			local p5 = r3[ox]
-			local p6 = r3[ox+1]
+			local p1 = colorMap[r1[ox]]
+			local p2 = colorMap[r1[ox+1]]
+			local p3 = colorMap[r2[ox]]
+			local p4 = colorMap[r2[ox+1]]
+			local p5 = colorMap[r3[ox]]
+			local p6 = colorMap[r3[ox+1]]
 			if p1 == p2 and p2 == p3 and p3 == p4 and p4 == p5 and p5 == p6 then
 				local c = colorChar[p1]
 				blitC1[x] = c
